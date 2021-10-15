b3f1f3f6a944b12130c250a6ce37b79a12d10ce0
Jorge Andrés Mejía <jorgemejia2501@gmail.com>
2021-10-15 13:31:01 -0600

    Fixed db

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,12 +3,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_DATABASE!,
-  process.env.DB_USER!,
-  process.env.DB_PASS!,
-  {
-    host: process.env.DB_HOST!,
-    dialect: "postgres",
-  }
-);
+export const sequelize = new Sequelize(process.env.DB_URI as string);
