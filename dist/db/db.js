0288f3003078bc35ba10bfa02ecff90da76b1cc8
Jorge Andrés Mejía <jorgemejia2501@gmail.com>
2021-10-15 14:11:06 -0600

    Fixed db 3.0

--- dist/db/db.js
+++ dist/db/db.js
@@ -7,4 +7,13 @@ exports.sequelize = void 0;
 var sequelize_1 = require("sequelize");
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-exports.sequelize = new sequelize_1.Sequelize(process.env.DB_URI);
+exports.sequelize = new sequelize_1.Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    ssl: true,
+    dialectOptions: {
+        ssl: {
+            require: true,
+        },
+    },
+});
--- src/db/db.ts
+++ src/db/db.ts
@@ -3,4 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_URI as string);
+export const sequelize = new Sequelize(
+  process.env.DB_DATABASE!,
+  process.env.DB_USER!,
+  process.env.DB_PASS!,
+  {
+    host: process.env.DB_HOST!,
+    dialect: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
+  }
+);
